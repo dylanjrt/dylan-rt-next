f18ce236408e65c1c99e6fd6a0ab1cb2e6db17a5
Dylan Rochon-Terry <dylan@ecovative.com>
2025-04-15 12:19:54 -0400

    Call date formatting function from utils for home page

--- src/app/page.js
+++ src/app/page.js
@@ -2,32 +2,11 @@ import { getHomePageContent } from "@/sanity/queries";
 import { PortableText } from "next-sanity";
 import { components } from "@/sanity/portableTextComponents";
 import dynamic from "next/dynamic";
+import { formatDateTime } from "./utils/dateFormat";
 
 const Blender3DComponent = dynamic(() => import("./components/Blender3D"), {
   loading: () => <div className="h-[400px] w-full bg-gray-100" />,
 });
-
-function formatDateTime(dateString) {
-  const date = new Date(dateString);
-  const hour = date.getHours();
-
-  // Determine time of day
-  let timeOfDay;
-  if (hour < 12) timeOfDay = "morning";
-  else if (hour < 17) timeOfDay = "afternoon";
-  else timeOfDay = "evening";
-
-  // Format the date and convert to lowercase
-  const formattedDate = date
-    .toLocaleDateString("en-US", {
-      month: "long",
-      day: "numeric",
-      year: "numeric",
-    })
-    .toLowerCase();
-
-  return `${timeOfDay} of ${formattedDate}`;
-}
 
 export default async function Home() {
   const homeData = await getHomePageContent();
