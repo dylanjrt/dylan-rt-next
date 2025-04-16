f3e633a2fa164d77e324233e0c7a31ba7de90ad5
Dylan Rochon-Terry <dylan@ecovative.com>
2025-04-16 14:46:04 -0400

    Use proper .ico file instead of .png for favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,7 +25,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.png" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
