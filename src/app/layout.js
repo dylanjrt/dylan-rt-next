fbd48464603620f6f102f834448fd130dd2b0f69
Dylan Rochon-Terry <dylan@ecovative.com>
2025-04-16 00:24:51 -0400

    Hide 'Thoughts' page for now

--- src/app/layout.js
+++ src/app/layout.js
@@ -421,7 +421,7 @@ export default function RootLayout({ children }) {
                       </div>
                     </div>
                   </Link>
-                  <Link
+                  {/* <Link
                     href="/thoughts"
                     className="border-transparent text-gray-500 hover:text-gray-700 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
                   >
@@ -559,7 +559,7 @@ export default function RootLayout({ children }) {
                         THOUGHTS
                       </div>
                     </div>
-                  </Link>
+                  </Link> */}
                 </div>
               </div>
             </div>
@@ -578,12 +578,12 @@ export default function RootLayout({ children }) {
                 >
                   Music
                 </Link>
-                <Link
+                {/* <Link
                   href="/thoughts"
                   className="text-gray-500 hover:bg-gray-50 hover:text-gray-700 block px-3 py-2 rounded-md text-base font-medium"
                 >
                   Thoughts
-                </Link>
+                </Link> */}
               </div>
             </div>
           </nav>
